e2159b11d0be97bbb4201669b2cd79981987a61f
Yoga Hapriana <thenelse.v2@gmail.com>
2023-12-11 16:04:28 +0700

    Basic Authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,22 @@
 class CommentsController < ApplicationController
-    def create
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.create(comment_params)
-      redirect_to article_path(@article)
-    end
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
   
-    def destroy
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.find(params[:id])
-      @comment.destroy
-      redirect_to article_path(@article), status: :see_other
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
     end
-  
-    private
-      def comment_params
-        params.require(:comment).permit(:commenter, :body, :status)
-      end
 end
   
